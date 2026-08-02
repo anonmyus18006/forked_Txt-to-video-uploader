7dd8cdd448864e905144fdc502d10b89849c1d46
Anonmyus18006 <basantdropyear@gmail.com>
2026-08-02 12:01:23 +0530

    Update environment variable defaults in vars.py

--- vars.py
+++ vars.py
@@ -3,18 +3,18 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "28221704"))
-API_HASH = environ.get("API_HASH", "a0163f47bc5262a3106b7858720a09fb")
-BOT_TOKEN = environ.get("BOT_TOKEN", "8842602749:AAECqZU_dC4mdqrymIb7cmA8HZqylD144tM")
+API_ID = int(environ.get("API_ID"))
+API_HASH = environ.get("API_HASH")
+BOT_TOKEN = environ.get("BOT_TOKEN")
 
-OWNER = int(environ.get("OWNER", "8321801403"))
+OWNER = int(environ.get("OWNER", "8076165805"))
 CREDIT = environ.get("CREDIT", "𝙎𝘼HIL 𝘽𝙊𝙏𝙎")
 cookies_file_path = os.getenv("cookies_file_path", "youtube_cookies.txt")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '8321801403').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '8076165805').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '8321801403').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '8076165805').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
